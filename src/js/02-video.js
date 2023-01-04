68c713160912f6b234a45f256b45f70d47ba0054
Olena Denysenko <helenazinchenko@ukr.net>
2023-01-04 21:20:19 +0200

    Task-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALE_STORAGE_KEY = 'videoplayer-current-time';
+
+const timeOfPlaying = localStorage.getItem(LOCALE_STORAGE_KEY);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(LOCALE_STORAGE_KEY, JSON.stringify(data));
+
+  // console.log(JSON.stringify(data));
+
+  player
+    .setCurrentTime(timeOfPlaying)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+const throttle = require('lodash.throttle');
 
+const feedbackFormEl = document.querySelector('.feedback-form');
+const userData = {};
+
+function autofillContactForm() {
+  try {
+    const userDataFromLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+
+    //якщо localeStorage порожній нічого робити не потрібно
+    if (userDataFromLS === null) {
+      return;
+    }
+
+    //перебираємо об'єкт циклом та робимо автозаповнення зі сховища
+    for (const key in userDataFromLS) {
+      feedbackFormEl.elements[key].value = userDataFromLS[key];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+autofillContactForm(); //викликається одразу як запускається скрипт
+
+function onFeedbackFormInput(e) {
+  const { target } = e;
+  const name = target.name;
+  const value = target.value;
+  userData[name] = value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}
+
+function onFeedbackFormSubmit(e) {
+  e.preventDefault();
+  feedbackFormEl.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(userData);
+}
+
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
+feedbackFormEl.addEventListener('input', throttle(onFeedbackFormInput, 500));
