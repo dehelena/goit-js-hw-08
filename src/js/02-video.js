7fcbece13d94a394e4dae69a6f3db7555b5e169a
Olena Denysenko <helenazinchenko@ukr.net>
2023-01-06 22:42:06 +0200

    fixed task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -13,19 +13,4 @@ function onPlay(data) {
   localStorage.setItem(LOCALE_STORAGE_KEY, JSON.stringify(data.seconds));
 }
 
-player
-  .setCurrentTime(timeOfPlaying)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(timeOfPlaying);
